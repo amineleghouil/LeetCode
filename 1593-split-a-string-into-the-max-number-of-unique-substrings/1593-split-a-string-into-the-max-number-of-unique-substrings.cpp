7c6c2748d447b143b3b956eab4d111302e8248c7
amine leghouil <amineleghouil@gmail.com>
2024-10-21 21:50:22 +0100

    Time: 217 ms (66.86%), Space: 47.9 MB (67.72%) - LeetHub

---
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxUniqueSplit(string s) {
+        unordered_set<string> seen;
+        return backtrack(s, 0, seen);
+    }
+    int backtrack(const string &s, int start, unordered_set<string> &seen) {
+        if (start == s.size()) {
+            return 0;
+        }
+
+        int maxSplits = 0;
+        string current = "";
+        for (int i = start; i < s.size(); ++i) {
+            current += s[i];
+            if (seen.find(current) == seen.end()) {
+                seen.insert(current);
+                maxSplits = max(maxSplits, 1 + backtrack(s, i + 1, seen));
+                seen.erase(current); 
+            }
+        }
+
+        return maxSplits;
+    }
+};
