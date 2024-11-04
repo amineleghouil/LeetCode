10f1c15dae744a697711c8daef4f9f244d4966a6
amine leghouil <amineleghouil@gmail.com>
2024-11-04 01:43:16 +0100

    Time: 23 ms (76.07%), Space: 31.3 MB (5.03%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.cpp
+++ 3163-string-compression-iii/3163-string-compression-iii.cpp
@@ -3,20 +3,20 @@ public:
     string compressedString(string s) {
         vector<pair<char,char>> v;
         v.push_back({s[0],'1'});
+        string s1="";
         int n=0;
         for(int i=1;i<s.size();++i){
             if(s[i]==v[n].first && v[n].second<'9')
             v[n].second++;
             else{
+                s1.push_back(v[n].second);
+                s1.push_back(v[n].first);
                 v.push_back({s[i],'1'});
                 n++;
             }
         }
-        string s1="";
-        for(int i=0;i<=n;i++){
-            s1.push_back(v[i].second);
-            s1.push_back(v[i].first);
-        }
+        s1.push_back(v[n].second);
+        s1.push_back(v[n].first);
         return s1;
     }
 };
