0ad93c2d596e56fb773cc00a753a00854c21ffc5
amine leghouil <amineleghouil@gmail.com>
2024-11-07 11:57:12 +0100

    Time: 31 ms (60.45%), Space: 60.3 MB (14.16%) - LeetHub

---
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -0,0 +1,24 @@
+#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx")
+#pragma GCC optimize ("O3", "unroll-loops")
+#pragma GCC optimize ("-ffloat-store")
+
+auto init = []() { 
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    return 0;
+} ();
+class Solution {
+public:
+    int largestCombination(vector<int>& candidates) {
+         int s = 0;
+        for(int i = 0; i < 24; i++) {
+            int x = 1 << i; 
+            int cnt = 0; 
+            for(int num : candidates) {
+                if (num & x) ++cnt; 
+            }
+            s = max(s, cnt);
+        }
+        return s;
+    }
+};
