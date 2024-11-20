f50ee87b249aa3240a02a94d890a9ba615b6ecdc
amine leghouil <amineleghouil@gmail.com>
2024-11-20 07:55:07 +0100

    Time: 21 ms (40.00%), Space: 11.8 MB (96.89%) - LeetHub

---
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
@@ -0,0 +1,19 @@
+class Solution {
+ public:
+  int takeCharacters(string s, int k) {
+    const int n = s.length();
+    int ans = n;
+    vector<int> v(3);
+    for (const char c : s)
+      v[c - 'a']++;
+    if (v[0] < k || v[1] < k || v[2] < k)
+      return -1;
+    for (int l = 0, r = 0; r < n; ++r) {
+      v[s[r] - 'a']--;
+      while (v[s[r] - 'a'] < k)
+        v[s[l++] - 'a']++;
+      ans = min(ans, n - (r - l + 1));
+    }
+    return ans;
+  }
+};
