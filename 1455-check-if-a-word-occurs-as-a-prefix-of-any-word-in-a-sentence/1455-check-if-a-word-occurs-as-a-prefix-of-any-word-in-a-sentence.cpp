17d6d613b7ca8871de123d0b9a2c3ff6af168acb
amine leghouil <amineleghouil@gmail.com>
2024-12-02 11:41:01 +0100

    Time: 0 ms (100.00%), Space: 8.4 MB (6.42%) - LeetHub

---
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int isPrefixOfWord(string sentence, string searchWord) {
+        stringstream s(sentence);
+        string w;
+        int i = 1; 
+        while (s>>w) {
+            if (w.find(searchWord)==0) {
+                return i;
+            }
+            i++;
+        }
+        
+        return -1;
+    }
+};
