8233a67257c84c9704e609a3236fb83b27b3543e
amine leghouil <amineleghouil@gmail.com>
2024-12-11 14:47:07 +0100

    Time: 54 ms (55.89%), Space: 105.1 MB (51.16%) - LeetHub

---
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumBeauty(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int i = 0; 
+        int maxi = 0;
+        for (int j = 0; j<n; ++j) {
+            while (nums[j]-nums[i]>2*k) {
+                i++;
+            }
+            maxi = max(maxi,j-i+1);
+        }
+        return maxi;
+    }
+};
