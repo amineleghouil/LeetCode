1c20a47f74adb8dd0d18b22bb0c58b48cb28193b
amine leghouil <amineleghouil@gmail.com>
2025-05-18 22:39:38 +0100

    Time: 1131 ms, Memory: 180.1 MB - LeetHub

--- 5317-138-1584-min-cost-to-connect-all-points/5317-138-1584-min-cost-to-connect-all-points.py
+++ 5317-138-1584-min-cost-to-connect-all-points/5317-138-1584-min-cost-to-connect-all-points.py
@@ -1,25 +1,25 @@
-import heapq
-from typing import List
-
 class Solution:
-    def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        n = len(points)
-        visited = [False] * n
-        min_heap = [(0, 0)]  
-        total_cost = 0
-        edges_used = 0
-
-        while edges_used < n:
-            cost, u = heapq.heappop(min_heap)
-            if visited[u]:
+    def minCostConnectPoints(self, p: List[List[int]]) -> int:
+        n = len(p)
+        if n <= 1:
+            return 0
+        l = [[] for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                    continue
+                dist = abs(p[i][0] - p[j][0]) + abs(p[i][1] - p[j][1])
+                l[i].append((dist, j))
+        s = 0
+        visited = set()
+        min_heap = [(0, 0)]
+        while min_heap and len(visited) < n:
+            dist, u = heapq.heappop(min_heap)
+            if u in visited:
                 continue
-            visited[u] = True
-            total_cost += cost
-            edges_used += 1
-
-            for v in range(n):
-                if not visited[v]:
-                    dist = abs(points[u][0] - points[v][0]) + abs(points[u][1] - points[v][1])
+            visited.add(u)
+            s += dist
+            for dist, v in l[u]:
+                if v not in visited:
                     heapq.heappush(min_heap, (dist, v))
-
-        return total_cost
+        return s
