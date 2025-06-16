2afba1dcfbe94ae6cd7c9186a4f288a3aab8f7e7
amine leghouil <amineleghouil@gmail.com>
2025-06-16 18:22:48 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maximumDifference(vector<int>& nums) {
+        int s = nums[1]-nums[0];
+        int m=min(nums[1],nums[0]);
+        for (int i=2;i<nums.size();++i){
+            m=min(m,nums[i-1]);
+            s=max(s,nums[i]-m);}
+        return s;
+    }
+};
