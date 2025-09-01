637b7b1f2b78e0af8859cec0c2d91e050766e072
amine leghouil <amineleghouil@gmail.com>
2025-09-01 17:52:20 +0100

    Time: 275 ms (85.96%), Space: 98 MB (47.37%) - LeetHub

--- 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.cpp
+++ 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
-        auto gain = [](double pass, double total) {
-            return (pass + 1) / (total + 1) - pass / total;
-        };
-
-        priority_queue<pair<double, pair<int, int>>> maxHeap;
-
-        double sum = 0.0;
-
-        for (const auto& cls : classes) {
-            int pass = cls[0], total = cls[1];
-            sum += (double)pass / total;  
-            maxHeap.push({gain(pass, total), {pass, total}});
+        using p = pair<double, int>;
+        priority_queue<p> pq;
+        for (int i = 0; i < classes.size(); ++i) {
+            double curr = (double)classes[i][0] / classes[i][1];
+            double newPr = (double)(classes[i][0] + 1) / (classes[i][1] + 1);
+            pq.push({newPr - curr, i});
         }
 
-        for (int i = 0; i < extraStudents; ++i) {
-            auto [currentGain, data] = maxHeap.top(); maxHeap.pop();
-            int pass = data.first, total = data.second;
 
-            sum -= (double)pass / total;
-            pass += 1;
-            total += 1;
-            sum += (double)pass / total;
-
-            maxHeap.push({gain(pass, total), {pass, total}});
+        while (extraStudents--) {
+            auto it = pq.top();
+            pq.pop();
+            int i = it.second;
+            classes[i][0] += 1;
+            classes[i][1] += 1;
+            double curr = (double)classes[i][0] / classes[i][1];
+            double newPr = (double)(classes[i][0] + 1) / (classes[i][1] + 1);
+            pq.push({newPr - curr, i});
         }
 
-        return sum / classes.size();
+        double ans = 0.0;
+        for (auto& it : classes) {
+            ans += (double)it[0] / it[1];
+        }
+
+        return ans / classes.size();
     }
 };
