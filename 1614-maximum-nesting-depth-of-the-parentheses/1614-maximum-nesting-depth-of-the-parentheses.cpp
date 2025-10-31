b028eca7390e8215b0445331b1febbc8a2cab629
amine leghouil <mohamedamin.leghouil@isimg.tn>
2025-10-31 05:07:14 +0100

    Time: 0 ms (100%), Space: 8.5 MB (9.41%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        stack<char> st;
+        int ans = 0;
+        for(int i = 0 ; i< s.size();++i){
+            if(s[i]=='('){st.push('(');
+            ans = max(ans, (int)st.size());}
+            else if(s[i]==')') st.pop();
+        }
+        return ans ;
+    }
+};
