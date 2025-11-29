0312bda7266acfde6a5bc08d6198e74e38f86220
amine leghouil <mohamedamin.leghouil@isimg.tn>
2025-11-29 23:24:11 +0100

    Time: 0 ms (100%), Space: 45.2 MB (94.68%) - LeetHub

--- 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.cpp
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        int n =0;
-        for(int i=0;i<nums.size();++i)n+=nums[i];
-        return n%=k;  
+        long long sum = 0;
+        for(int x : nums) sum += x;
+        return sum % k;
     }
 };
